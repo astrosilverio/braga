9110811755cf4657a2fdcb50294d7f23866df74a
Katie Silverio <katie.silverio@venmo.com>
2015-06-26 17:03:27 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='braga',
+      version='0.1',
+      description='Toy Entity-Component system',
+      url='http://github.com/astrosilverio/braga',
+      author='astrosilverio',
+      author_email='astrosilverio@gmail.com',
+      license='MIT',
+      packages=['braga'],
+      zip_safe=False)
