37dc7082d5753e8d8616d653a41ba4ba094c42c1
Katie Silverio <katie.silverio@venmo.com>
2015-07-13 12:10:18 -0400

    assemblage!

--- braga/models.py
+++ braga/models.py
@@ -1,7 +1,10 @@
+import uuid
+
+
 class Entity(object):
 
-    def __init__(self, uuid):
-        self._uuid = uuid
+    def __init__(self):
+        self._uuid = uuid.uuid4()
         self.components = set()
 
     @property
@@ -26,5 +29,20 @@ class Entity(object):
 
 
 class Component(object):
+
     def __repr__(self):
         return type(self).__name__
+
+
+class Assemblage(object):
+
+    def __init__(self):
+        self.component_types = dict()
+
+    def add_component(self, component_type, init_args=None):
+        self.component_types[component_type] = init_args
+
+    def make(self):
+        entity = Entity()
+        entity.components |= set([k(v) if v is not None else k() for k, v in self.component_types.iteritems()])
+        return entity
