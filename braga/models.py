2489c1f8acde1105b6145d0586c86aa1b15f11b1
Katie Silverio <katie.silverio@venmo.com>
2015-09-07 14:50:02 -0400

    add aspect to System

--- braga/models.py
+++ braga/models.py
@@ -53,8 +53,9 @@ class System(object):
 
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self):
+    def __init__(self, aspect=None):
         self.thread = None
+        self.aspect = aspect if aspect else Aspect()
 
     def start(self):
         self.thread = threading.Thread(name=type(self).__name__, target=self.update)
