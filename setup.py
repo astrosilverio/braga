74718094c1f6a61d88a7aab46b9121245cf04b96
Katie Silverio <katie.silverio@venmo.com>
2016-01-23 19:49:51 -0500

    grrr switch to semantic versioning, start at 4 bc I messed it up the first time

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='braga',
-      version='0.3',
+      version='0.4.0',
       description='Toy Entity-Component system',
       url='http://github.com/astrosilverio/braga',
       author='astrosilverio',
