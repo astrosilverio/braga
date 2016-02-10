7c2867f003f69faeea2fae739bf17a4df8e42a6a
Katie Silverio <katie.silverio@venmo.com>
2016-02-10 18:54:18 -0500

    try sys.path

--- braga/__init__.py
+++ braga/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from braga.entity import Entity
 from braga.component import Component
 from braga.system import System
@@ -5,4 +7,6 @@ from braga.assemblage import Assemblage
 from braga.aspect import Aspect
 from braga.world import World
 
+sys.path.append("..")
 from examples import duel
+sys.path.remove("..")
