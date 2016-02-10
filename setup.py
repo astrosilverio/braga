24b266a541fe8726b27a451181cd9cd1cfd27f8d
Katie Silverio <katie.silverio@venmo.com>
2016-02-09 21:28:59 -0500

    make version variable

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='braga',
-      version='0.5.5',
-      description='Toy Entity-Component system',
-      url='http://github.com/astrosilverio/braga',
-      author='astrosilverio',
-      author_email='astrosilverio@gmail.com',
-      license='MIT',
-      packages=['braga'],
-      zip_safe=False)
+__version__ = '0.5.6'
+
+if __name__ == '__main__':
+    setup(name='braga',
+          version=__version__,
+          description='Entity-Component system',
+          url='http://github.com/astrosilverio/braga',
+          author='astrosilverio',
+          author_email='astrosilverio@gmail.com',
+          license='MIT',
+          packages=['braga'],
+          zip_safe=False)
